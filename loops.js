bf6cc82e45a349e83ebf91b10bf1f6cc7e81790d
coartney22@gmail.com <coartney22@gmail.com>
2018-02-22 02:12:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,10 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++) {
-    if (i === 1) break {
+    if (i === 1){
     return `I am 1 strange loop.`}
-    else {
+    else if (1 < i > 26) {
       return `I am ${i} strange loops.`
     }
+    else break;
   }
 }
