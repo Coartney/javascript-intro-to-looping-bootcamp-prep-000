facb4b94eeb2146a2eaacbab4268c4619412e180
coartney22@gmail.com <coartney22@gmail.com>
2018-02-22 02:35:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,8 +4,8 @@ function forLoop(array){
     array.push(`I am 1 strange loop.`)}
     else {
     array.push(`I am ${i} strange loops.`)}
-    return array;
   }
+  return array;
 }
 function whileLoop(n){
   while (n > 0){
