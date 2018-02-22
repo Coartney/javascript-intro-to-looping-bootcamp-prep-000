6ce524e2e6344649db74d0ff8a2a7c98aeaee606
coartney22@gmail.com <coartney22@gmail.com>
2018-02-22 02:48:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -14,9 +14,6 @@ function whileLoop(n){
   return "done";
 }
 function doWhileLoop(array){
-  function maybeTrue() {
-  return Math.random() >= 0.5
-  }
   do {
   array.length > 0 && maybeTrue();
   } while (maybeTrue());
